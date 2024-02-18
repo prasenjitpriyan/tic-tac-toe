8ed1785f311e3b8ff182bec952b748d078d5d726
PRASENJIT DAS <constprasenjit@PRASENJITs-MacBook-Air.local>
2024-02-18 22:07:12 +0530

    Backend remove

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: true,
-    strictPort: true,
-    port: 3000,
-  },
   plugins: [react()],
 })
