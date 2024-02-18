1bd90c087c08b07294c4ea8bf5019492195d6c5e
PRASENJIT DAS <constprasenjit@PRASENJITs-MacBook-Air.local>
2024-02-18 21:15:00 +0530

    Vite modified

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      '/server': {
-        target: 'http://localhost:3000',
-        secure: false
-      },
-    },
+    host: true,
+    strictPort: true,
+    port: 3000,
   },
   plugins: [react()],
 })
