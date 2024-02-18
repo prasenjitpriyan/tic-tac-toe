c48cfdd008ecf19fe3f635e26cc5611306d92aca
PRASENJIT DAS <constprasenjit@PRASENJITs-MacBook-Air.local>
2024-02-18 22:27:32 +0530

    Render added

--- src/App.jsx
+++ src/App.jsx
@@ -129,7 +129,7 @@ const App = () => {
     const username = result.value;
     setPlayerName(username);
 
-    const newSocket = io("http://localhost:3000", {
+    const newSocket = io("https://booklistback.onrender.com", {
       autoConnect: true,
     });
 
