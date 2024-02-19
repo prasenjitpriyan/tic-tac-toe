2fd4318eaece8c339ff022144ac2d6c939ce3c26
Prasenjit Das <132381731+prasenjitpriyan@users.noreply.github.com>
2024-02-19 21:05:41 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ const allUsers = {};
 const allRooms = [];
 
 app.use(express.static(path.join(__dirname, '../../build')));
-app.get('/', (req, res, next) => res.sendFile(__dirname + './index.html'));
 
 io.on("connection", (socket) => {
   allUsers[socket.id] = {
